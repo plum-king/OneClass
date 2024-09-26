e684ba7f4143c994c924ab13215441418c0b847d
plum-king <jeesoo0908@gmail.com>
2024-09-26 14:21:51 +0900

    Initial Commit

---
+++ src/main/java/com/example/OneClass/OneClassApplication.java
@@ -0,0 +1,13 @@
+package com.example.OneClass;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OneClassApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OneClassApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/OneClass/OneClassApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.OneClass;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OneClassApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
